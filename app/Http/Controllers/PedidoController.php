fb4cfd069450b4d76066a22c087c2e226c16de15
Beto <betobatista_souza@hotmail.com>
2023-12-20 12:02:46 -0300

    Inicializando o projeto Laravel

---
+++ app/Http/Controllers/PedidoController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PedidoController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+  
+    }
+
+    public function show($id)
+    {
+  
+    }
+
+    public function update(Request $request, $id)
+    {
+       
+    }
+
+    public function destroy($id)
+    {
+       
+    }
+
+    public function realizarPedido(Request $request)
+    {
+      
+    }
+
+    public function calcularFrete(Request $request)
+    {
+        
+    }
+}
---
+++ database/migrations/2023_12_20_040658_create_pedidos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('usuario_id');
+            $table->decimal('total', 10, 2);
+            $table->enum('status', ['pendente', 'processando', 'enviado', 'entregue']);
+            $table->timestamps();
+
+            // Chave estrangeira para usuário
+            $table->foreign('usuario_id')->references('id')->on('usuarios');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
